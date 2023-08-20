d770db66d11441a19ff2f01c8fdfbd677e580733
xTycoonFi <tycoonweb@proton.me>
2023-08-20 21:49:11 +0200

    take this to start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { ethers } = require('ethers');
 
-const rpc = 'https://base.meowrpc.com';
+let rpc = 'https://base.meowrpc.com';
 const provider = new ethers.JsonRpcProvider(rpc);
 
 const privateKey = process.env.PRIVATE_KEY;
@@ -14,14 +14,36 @@ const contract = new ethers.Contract(friendContractAddress, friendABI, wallet);
 
 async function start() {
     try {
-        contract.on('Trade', (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
+        contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
             console.log('Trade Event:', {
                 trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply
             });
+
+            if (ethAmount === 0n) {
+                console.log('ethAmount is 0, calling buyShares function...');
+
+                const sharesSubject = subject;
+                const amount = 1;
+
+                try {
+                    const tx = await contract.buyShares(sharesSubject, amount, {
+                        value: 70000000000000, //VALUE FOR 1
+                    });
+                    await tx.wait();
+                    console.log('buyShares function called successfully.');
+                    start();
+                } catch (error) {
+                    console.error('Error calling buyShares:', error);
+                    start();
+                }
+            }
         });
+
         console.log('Listening for Trade events...');
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error with RPC');
+        console.log('finished?')
+
     }
 }
 
