f4cb5db793271b1be65bd6635be5dfd8bf914733
spuriousdragon <spuriousdragon@gmail.com>
2023-08-22 01:44:38 +0200

    latest

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const { ethers } = require('ethers');
-
 const fs = require('fs');
 
-let rpc = 'https://base.meowrpc.com';
+const rpc = 'https://base.meowrpc.com';
 const provider = new ethers.JsonRpcProvider(rpc);
 
 const privateKey = process.env.PRIVATE_KEY;
@@ -30,59 +29,47 @@ async function saveSubject(subject) {
     }
 }
 
-async function start() {
-    
+console.log('Listening for Trade events...');
+contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
     try {
-        contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
-             
+        if (ethAmount === 0n) {
+            console.log('ethAmount is 0, calling buyShares function...');
 
-        console.log('Listening for Trade events...');
-            if (ethAmount === 0n) {
+            const sharesSubject = subject;
+            const amount = 1;
 
-                // console.log('Trade Event:', {
-                //trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply
-            //});
-                console.log('ethAmount is 0, calling buyShares function...');
-            
-                const sharesSubject = subject;
-                const amount = 1;
+            saveSubject(subject);
 
-                saveSubject(subject);
-                try {
-                    const txObject = {
-                        to: friendContractAddress,
-                        value: 70000000000000,
-                       data: contract.interface.encodeFunctionData('buyShares', [sharesSubject, amount]),
-          
-                    };
-                  
-                   const gasLimit = await wallet.estimateGas(txObject);
-                    console.log(gasLimit);
-                    const tx = await contract.buyShares(sharesSubject, amount, {
-                        value: 70000000000000, //VALUE FOR 1
-                        gasLimit: gasLimit,
-                    });
-                    await tx.wait();
-                    console.log('buyShares function called successfully.');
-                     await new Promise(resolve => setTimeout(resolve, 5000));
-                    
-                } catch (error) {
-                    console.error('Error calling buyShares:', error);
-                     await new Promise(resolve => setTimeout(resolve, 5000));
-                     
-                }
+            try {
+                const txObject = {
+                    to: friendContractAddress,
+                    value: 70000000000000,
+                    data: contract.interface.encodeFunctionData('buyShares', [sharesSubject, amount]),
+                };
+
+                const gasLimit = await wallet.estimateGas(txObject);
+                console.log(gasLimit);
+
+                const tx = await contract.buyShares(sharesSubject, amount, {
+                    value: 70000000000000,
+                    gasLimit: gasLimit,
+                });
+
+                await tx.wait();
+                console.log('buyShares function called successfully.');
+                await new Promise(resolve => setTimeout(resolve, 5000));
+            } catch (error) {
+                console.error('Error calling buyShares:', error);
+                await new Promise(resolve => setTimeout(resolve, 5000));
             }
-          
-        });
-
+        }
     } catch (error) {
-        console.error('Error with RPC');
-        await new Promise(resolve => setTimeout(resolve, 15000));
-        start(); 
+        console.error('Error with Trade event listener:', error);
     }
-  
-         
-}
+});
 
-start();
+// Handle errors that might occur
+// contract.on('error', (error) => {
+//     console.error('Error with contract.on:', error);
+// });
 
