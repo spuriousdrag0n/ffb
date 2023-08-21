a2fb7540c906543dc77e9206ac99900033d07243
spuriousdragon <spuriousdragon@gmail.com>
2023-08-21 08:55:30 +0200

    gas fix write to file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { ethers } = require('ethers');
 
+const fs = require('fs');
+
 let rpc = 'https://base.meowrpc.com';
 const provider = new ethers.JsonRpcProvider(rpc);
 
@@ -12,22 +14,50 @@ const friendABI = require('./ABI/friendABI.json');
 
 const contract = new ethers.Contract(friendContractAddress, friendABI, wallet);
 
+const savedSubjectsFilePath = 'saved_subjects.json';
+
+async function saveSubject(subject) {
+    try {
+        let existingSubjects = [];
+        if (fs.existsSync(savedSubjectsFilePath)) {
+            existingSubjects = JSON.parse(fs.readFileSync(savedSubjectsFilePath));
+        }
+        existingSubjects.push(subject);
+        fs.writeFileSync(savedSubjectsFilePath, JSON.stringify(existingSubjects, null, 2));
+        console.log('Subject saved to file.');
+    } catch (error) {
+        console.error('Error saving subject:', error);
+    }
+}
+
 async function start() {
     try {
         contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
-            console.log('Trade Event:', {
-                trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply
-            });
+             
 
             if (ethAmount === 0n) {
+                // console.log('Trade Event:', {
+                //trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply
+            //});
                 console.log('ethAmount is 0, calling buyShares function...');
-
+            
                 const sharesSubject = subject;
                 const amount = 1;
 
+                saveSubject(subject);
                 try {
+                    const txObject = {
+                        to: friendContractAddress,
+                        value: 70000000000000,
+                       data: contract.interface.encodeFunctionData('buyShares', [sharesSubject, amount]),
+          
+                    };
+                  
+                   const gasLimit = await wallet.estimateGas(txObject);
+                    console.log(gasLimit);
                     const tx = await contract.buyShares(sharesSubject, amount, {
                         value: 70000000000000, //VALUE FOR 1
+                        gasLimit: gasLimit,
                     });
                     await tx.wait();
                     console.log('buyShares function called successfully.');
@@ -37,6 +67,7 @@ async function start() {
                     start();
                 }
             }
+          
         });
 
         console.log('Listening for Trade events...');
