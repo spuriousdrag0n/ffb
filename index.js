f222c0a5b0e5383186e18ce531834504922fb841
spuriousdragon <spuriousdragon@gmail.com>
2023-09-06 23:24:52 +0200

    put a limit on wallet balance / fixed overwrite in clean subject

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ async function initializeProvider() {
 
 initializeProvider();
 
-
 const privateKey = process.env.PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey, provider);
 
@@ -47,54 +46,67 @@ async function saveSubject(subject) {
     }
 }
 
-console.log('Listening for Trade events...');
-contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
+const minEthBalance = 8000000000000000; // Minimum ETH balance required
+
+async function checkBalanceAndBuy() {
     try {
-        if (ethAmount === 0n) {
-            console.log('ethAmount is 0, calling buyShares function...');
-            saveSubject(subject);
+        const ethBalance = await provider.getBalance(wallet.address);
+        console.log(`Ethereum balance for wallet: ${ethBalance.toString()}`);
 
-            const sharesSubject = subject;
-            const amount = 1;
-
-
-            try {
-                const txObject = {
-                    to: friendContractAddress,
-                    value: 69000000000000,
-                    data: contract.interface.encodeFunctionData('buyShares', [sharesSubject, amount]),
-                };
-
-                const gasLimit = await wallet.estimateGas(txObject);
-                console.log(gasLimit);
-
-                const tx = await contract.buyShares(sharesSubject, amount, {
-                    value: 69000000000000,
-                    gasLimit: gasLimit + BigInt(7000),
-                });
-
-                await tx.wait();
-                await new Promise(resolve => setTimeout(resolve, 10000));
-                if (receipt.status === 1) {
-                    console.log('Transaction was successful.');
-                } else {
-                    console.log('Transaction failed.');
-                    await new Promise(resolve => setTimeout(resolve, 5000));
-                }
-                console.log('buyShares function called successfully.');
-                await new Promise(resolve => setTimeout(resolve, 5000));
-            } catch (error) {
-                console.error('Error calling buyShares:', error);
-                await new Promise(resolve => setTimeout(resolve, 5000));
-            }
+        if (ethBalance< minEthBalance) {
+           console.log('Ethereum balance is less than 0.008 ETH. Exiting the script.');
+           return; // Exit the script
         }
-    } catch (error) {
-        console.error('Error with Trade event listener:', error);
-    }
-});
 
-// Handle errors that might occur
-// contract.on('error', (error) => {
-//     console.error('Error with contract.on:', error);
-// });
+        console.log('Listening for Trade events...');
+        contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
+            try {
+                if (ethAmount === 0n) {
+                    console.log('ethAmount is 0, calling buyShares function...');
+                    saveSubject(subject);
+
+                    const sharesSubject = subject;
+                    const amount = 1;
+
+                    try {
+                        const txObject = {
+                            to: friendContractAddress,
+                            value: 69000000000000,
+                            data: contract.interface.encodeFunctionData('buyShares', [sharesSubject, amount]),
+                        };
+
+                        const gasLimit = await wallet.estimateGas(txObject);
+                        console.log(gasLimit);
+
+                        const tx = await contract.buyShares(sharesSubject, amount, {
+                            value: 69000000000000,
+                            gasLimit: gasLimit + BigInt(7000),
+                        });
+
+                        await tx.wait();
+                        await new Promise(resolve => setTimeout(resolve, 10000));
+                        if (tx.hash) {
+                            console.log('Transaction was successful.');
+                        } else {
+                            console.log('Transaction failed.');
+                            await new Promise(resolve => setTimeout(resolve, 5000));
+                        }
+                        console.log('buyShares function called successfully.');
+                        await new Promise(resolve => setTimeout(resolve, 5000));
+                    } catch (error) {
+                        console.error('Error calling buyShares:', error);
+                        await new Promise(resolve => setTimeout(resolve, 5000));
+                    }
+                }
+            } catch (error) {
+                console.error('Error with Trade event listener:', error);
+            }
+        });
+
+    } catch (error) {
+        console.error('Error checking balance and buying:', error);
+    }
+}
+
+checkBalanceAndBuy();
 
